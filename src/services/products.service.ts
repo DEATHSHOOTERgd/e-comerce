e53710b67014439246d1df835317f7374a8d9d42
DEATHSHOOTERgd <giampaolo.delgadoo@ug.edu.ec>
2024-12-04 17:17:11 -0500

    Test

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -7,6 +7,7 @@ const VITE_API_URL = import.meta.env.VITE_API_URL;
 export const getProducts = () => {
     const controller = loadAbort();
     return {
+      
       call: axios.get<Product[]>(`${VITE_API_URL}/products`, { signal: controller.signal }),
       controller
     };
